e505925f17e74b32975b9544f567d712da41aacf
Lewis Hodgson <lewishodgson@Lewiss-MacBook-Air.local>
2022-10-25 16:39:52 +0100

    website

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
-		<ScrollToTop />
-		<App />
-	</BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ScrollToTop />
+    <App />
+  </BrowserRouter>
 );
